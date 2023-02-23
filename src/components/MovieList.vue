03448295d3dac5fee9a6939feeb445a35859877b
royanagist <royanagist29@gmail.com>
2023-02-23 23:48:10 +0700

    adding swiper

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,5 +1,7 @@
 <script>
 import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { EffectCoverflow, Pagination } from "swiper";
 import MovieItem from "./MovieItem.vue";
 import ToggleTrending from "./ToggleTrending.vue";
 
@@ -7,6 +9,13 @@ export default {
   components: {
     ToggleTrending,
     MovieItem,
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [EffectCoverflow, Pagination],
+    };
   },
   data() {
     return {
@@ -41,21 +50,58 @@ export default {
   created() {
     this.fetchTrending();
   },
+  mounted() {
+    // const swiperSlide = document.querySelectorAll(".swiper-slide");
+    const swiperSlide = document.getElementsByClassName("swiper-slide");
+    console.log(swiperSlide);
+  },
 };
 </script>
 
 <template>
-  <div class="d-flex align-items-center mb-4">
+  <div class="d-flex align-items-center">
     <h1>Trending</h1>
     <ToggleTrending @switchTrending="switchTrending"></ToggleTrending>
   </div>
-  <div class="row justify-content-center">
-    <MovieItem
-      v-for="item in trendingList"
-      :key="item.id"
-      :title="item.title"
-      :poster_path="item.poster_path"
-      :release_date="item.release_date"
-    ></MovieItem>
-  </div>
+  <swiper
+    :effect="'coverflow'"
+    :grabCursor="true"
+    :centeredSlides="true"
+    :slidesPerView="'auto'"
+    :coverflowEffect="{
+      rotate: -10,
+      stretch: 0,
+      depth: 300,
+      modifier: 0.3,
+      slideShadows: false,
+    }"
+    :spaceBetween="10"
+    :pagination="{
+      clickable: true,
+    }"
+    :loop="true"
+    :breakpoints="{
+      '640': {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      '768': {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      '1024': {
+        slidesPerView: 7,
+        spaceBetween: 50,
+      },
+    }"
+    :modules="modules"
+  >
+    <swiper-slide v-for="item in trendingList" :key="item.id">
+      <MovieItem
+        :title="item.title"
+        :poster_path="item.poster_path"
+        :release_date="item.release_date"
+      ></MovieItem>
+    </swiper-slide>
+  </swiper>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import "swiper/css/bundle";
 
 const app = createApp(App);
 
